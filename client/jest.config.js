710216bd663ce7caa453ea41012adabce1bf9889
Adam <40166384+Xsychic@users.noreply.github.com>
2022-02-26 19:35:33 +0000

    Setup js coverage report

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+    preset: '@vue/cli-plugin-unit-jest',
+    verbose: true,
+    moduleFileExtensions: [
+        'js',
+        'json',
+        'vue'
+    ],
+    collectCoverage: true,
+    coverageDirectory: './tests/coverage',
+    collectCoverageFrom: ['src/components/*.{js,vue}', 'src/views/*.{js,vue}', '!/node_modules/'],
+    coverageReporters: ['text-summary']
 }
