cb2c9c56efd94979b44a96f5be42c104f07a893b
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-05 01:59:01 +0000

    Remove old fa package comments

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// import fontawesome icons
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faBell } from '@fortawesome/free-solid-svg-icons';
-
-// library.add(faBell);
-
 createApp(App)
     .use(router)
     .mount('#app')
