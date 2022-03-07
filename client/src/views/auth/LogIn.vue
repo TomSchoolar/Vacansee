44e1ed4315c5ffcd7c4d966839fca2d6396e7ca3
Cavan Day <cxd066@cs.bham.ac.uk>
2022-03-07 23:58:20 +0000

    Create login view

---
+++ client/src/views/auth/LogIn.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class='auth-page'>
+		<div class='auth-page-container'>
+			<p class='auth-page-logo'>Tindeed</p>
+			<input v-model='cred' placeholder='Email' />
+			<input v-model='cred' placeholder='Password' />
+			<button @click='login'>Log In</button>
+		</div>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+    import ref from 'vue';
+
+	const cred = ref([]);
+
+	const login = () => {
+		alert("logged in");
+	}
+</script>
+
+<style>
+	.auth-page {
+		width: 100vw;
+		height: 100vh;
+		position: relative;
+	}
+	
+	.auth-page-container {
+		width: 100%;
+		max-width: 500px;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		position: absolute;
+	}
+
+	.auth-page-container input {
+		color: --var(slate);
+		font-size: 18px;
+		line-height: 24px;
+	}
+
+	.auth-page-logo {
+		color: --var(blue);
+		font-weight: bold;
+		font-size: 36px;
+		line-height: 1;
+		text-transform: uppercase;
+		margin: 0 0 25px 0;
+	}
+</style>
