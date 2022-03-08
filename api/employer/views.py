72e46be88f34871d3307d8094d129331d75a5d16
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-08 23:36:45 +0000

    Create basic get api route for /e/vacancy

--- api/employer/views.py
+++ api/employer/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Vacancy
+from .serializers import VacancySerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def getEmployerIndex(request):
+    vacancies = Vacancy.objects.filter(UserId__exact=4)
+    serializer = VacancySerializer(vacancies, many=True)
+    return Response(serializer.data)
