0d62fd99eca2a49f9b390d06c83ba42281ed1356
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-09 01:01:31 +0000

    Populate basic fields in employer index template

--- api/employer/views.py
+++ api/employer/views.py
@@ -1,10 +1,43 @@
 from .models import Vacancy
+from employee.models import Application
 from .serializers import VacancySerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from employee.serializers import ApplicationSerializer
+
 
 @api_view(['GET'])
 def getEmployerIndex(request):
-    vacancies = Vacancy.objects.filter(UserId__exact=4)
-    serializer = VacancySerializer(vacancies, many=True)
-    return Response(serializer.data)
+    # get vacancies matching userid
+    vacanciesSet = Vacancy.objects.filter(UserId__exact=4)
+    vacancySerializer = VacancySerializer(vacanciesSet, many=True)
+    vacancies = vacancySerializer.data
+
+    # get the number of new, accepted and rejected applications for each vacancy
+    for vacancy in vacancies:
+        applicationsSet = Application.objects.filter(VacancyId__exact=vacancy['VacancyId'])
+        applicationSerializer = ApplicationSerializer(applicationsSet, many=True)
+        applications = applicationSerializer.data
+
+        new = 0
+        accepted = 0
+        rejected = 0
+        total = 0
+
+        for application in applications:
+            status = application['ApplicationStatus']
+            total += 1
+
+            if status == 'PENDING':
+                new += 1
+            elif status == 'MATCHED':
+                accepted += 1
+            else:
+                rejected += 1
+        
+        vacancy['NewApplications'] = new
+        vacancy['AcceptedApplications'] = accepted
+        vacancy['RejectedApplications'] = rejected
+        vacancy['Applications'] = total
+
+    return Response(vacancies)
