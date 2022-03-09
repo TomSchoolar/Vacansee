d4a19e0a546d0f113c855a4247aec466113e0351
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-09 01:01:31 +0000

    Populate basic fields in employer index template

--- api/employer/models.py
+++ api/employer/models.py
@@ -25,6 +25,10 @@ class Vacancy(models.Model):
     PhoneNumber = models.CharField(max_length=15, blank=False)
     Email = models.EmailField(max_length=254, blank=False)
     Created = models.DateField(auto_now_add=True, blank=False)
+<<<<<<< HEAD
+=======
+
+>>>>>>> e14983f (Populate basic fields in employer index template)
 
 
 class Tag(models.Model):
