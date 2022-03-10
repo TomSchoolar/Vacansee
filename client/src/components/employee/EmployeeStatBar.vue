0e9ee16f212bb308cb36ec8f804fdbf3cc9326b7
Cavan Day <cxd066@cs.bham.ac.uk>
2022-03-10 16:18:42 +0000

    Upload employee status bar template

---
+++ client/src/components/employee/EmployeeStatBar.vue
@@ -0,0 +1,54 @@
+<script setup>
+    const props = defineProps(['stats']);
+    
+    const labels = {
+        totalApplications: 'Total Applications',
+        pendingApplications: 'Applications Pending',
+        matches: 'Matches'
+    };
+</script>
+
+
+
+<template>
+    <div class='stat-bar'>
+        <div class='stat-boxes'>
+            <div class='stat-box' v-for='[key, value] of Object.entries(stats)' :key='key'>
+                <h4 class='stat-title'>{{ labels[key] }}:</h4>
+                <h1 class='stat'>{{ value }}</h1>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+
+
+<style scoped>
+    .stat {
+        color: var(--red);
+        font-size: 50px;
+        margin: 0;
+    }
+
+    .stat-bar {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .stat-boxes {
+        width: 100%;
+        display: flex;
+        justify-content: space-evenly;
+        margin: 15px 0 30px 0;
+    }
+
+    .stat-title {
+        font-size: 18px;
+        margin: 8px 0;
+    }
+    
+
+</style>
