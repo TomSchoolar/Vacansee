d381969c5ff5eeabc54434ca63a946ef059cce70
sapphyree <sonexix@gmail.com>
2022-03-11 12:06:37 +0000

    Create profile and vacancy card partials.

---
+++ client/src/components/partials/ProfileCard.vue
@@ -0,0 +1,122 @@
+<script setup>  
+    const props = defineProps(['firstName', 'pronouns', 'location', 'topicSentence', 'skills', 'experience', 'qualifications']);
+</script>
+
+<template>
+    <div class='card'>
+        <div class='info'>
+            <p class='name'>{{ firstName }} <span class='pronouns'>({{ pronouns }})</span></p>
+            <p class='location'>Based in {{ location }}</p>
+        </div>
+        <div class='description'>
+            <p>{{ topicSentence }}</p>
+        </div>
+        <span class='card-section'>Notable Skills:</span>
+        <div class='skills' v-for='skill in skills' v-bind:key='skill'>
+            <table>
+                <tr>
+                    <th>- {{ skill }}</th>
+                </tr>
+            </table>
+        </div>
+        <span class='card-section'>Experience:</span>
+        <div class='experience' v-for='xp in experience' v-bind:key='xp'>
+            <table>
+                <tr>
+                    <th>- {{ xp.title }}</th>
+                    <th><span class='date'>{{ xp.startDate }} - {{ xp.endDate }}</span></th>
+                </tr>
+            </table>
+        </div>
+        <span class='card-section'>Qualifcations:</span>
+        <div class='qualifications' v-for='qual in qualifications' v-bind:key='qual'>
+            <table>
+                <tr>
+                    <th>- {{qual}}</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    th {
+        font-weight: normal;
+        padding-bottom: 0px;
+        padding-top: 0px;
+    }
+
+    .card {
+        font-weight: normal;
+        height: 500px;
+        width: 400px;
+        border: 2px solid #555;
+        border-radius: 15px;
+        margin: 12px 0;
+        align-items: center;
+        text-align: left;
+        justify-content: space-between;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-top: 5px;
+    }
+
+    .card-section {
+        font-weight: bold;
+    }
+
+    .description p {
+        margin: 0;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+    .experience .date {
+        font-style: italic;
+        color: var(--slate);
+        font-size: 14px;
+    }
+
+    .info p {
+        margin: 0px;
+    }
+
+    .name { 
+        font-weight: bold;
+        font-size: 24px;
+        margin: 0px;
+        padding: 10px 0px 0px 0px;
+    }
+
+    .job-title {
+        font-style: italic;
+    }
+
+    .location {
+        font-style: italic;
+    }
+    
+    .pronouns {
+        font-weight: normal;
+        font-size: 16px;
+        font-style: italic;
+        color: var(--slate);
+    }
+
+    .skills {
+        font-weight: normal;
+    }
+
+    .tags {
+        font-size: 250%;
+        padding-left: 15px;
+        padding-top: 0px;
+        padding-right: 10px;
+    }
+
+    #favourite {
+        float: right;
+        padding: 15px;
+        font-size: 45px;
+    }
+</style>
---
+++ client/src/components/partials/VacancyCard.vue
@@ -0,0 +1,106 @@
+<script setup>  
+    const props = defineProps(['companyName', 'jobTitle', 'location', 'description', 'skills', 'experience', 'tags', 'favourited']);
+</script>
+
+<template>
+    <div class='card'>
+        <div class='company-info'>
+            <div class='company-name'>{{ companyName }}<i id='favourite' class='fas fa-star' v-show="favourited === 'True'"></i></div>
+            <p class='job-title'>{{ jobTitle }}</p>
+            <p class='location'>Based in {{ location }}</p>
+        </div>
+        <div class='description'>
+            <p>{{ description }}</p>
+        </div>
+        <span style='font-weight: bold;'>Necessary Skills:</span>
+        <div class='skills' v-for='skill in skills' v-bind:key='skill'>
+            <table>
+                <tr>
+                    <th>- {{skill}}</th>
+                </tr>
+            </table>
+        </div>
+        <span style='font-weight: bold;'>Experience:</span>
+        <div class='experience' v-for='xp in experience' v-bind:key='xp'>
+            <table>
+                <tr>
+                    <th>- {{xp}}</th>
+                </tr>
+            </table>
+        </div>
+        <span style='font-weight: bold;'>Requirements:</span>
+        <table>
+        <tr>
+            <th class='tags' v-for='tag in tags' v-bind:key='tag.id'>
+                <i :class=tag.icon></i>
+            </th>
+        </tr>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+    th {
+        font-weight: normal;
+        padding-bottom: 0px;
+        padding-top: 0px;
+    }
+
+    .card {
+        font-weight: normal;
+        height: 500px;
+        width: 400px;
+        border: 2px solid #555;
+        border-radius: 15px;
+        margin: 12px 0;
+        align-items: center;
+        text-align: left;
+        justify-content: space-between;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+
+    .company-info p {
+        margin: 0px;
+        padding: 5px 0px 0px 0px;
+    }
+
+    .company-name { 
+        font-weight: bold;
+        font-size: 24px;
+        margin: 0px;
+        padding: 10px 0px 0px 0px;
+    }
+
+    .description p {
+        margin: 0;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+    .job-title {
+        font-style: italic;
+    }
+
+    .location {
+        font-style: italic;
+    }
+
+    .skills {
+        font-weight: normal;
+    }
+
+    .tags {
+        font-size: 250%;
+        padding-left: 15px;
+        padding-top: 0px;
+        padding-right: 10px;
+    }
+
+    #favourite {
+        float: right;
+        padding: 15px;
+        font-size: 45px;
+        color: var(--red);
+    }
+
+</style>
