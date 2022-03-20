f75f76d4ccbd8725d748d1f84cbe9ff8fad25432
Thomas Schoolar <txs090@cs.bham.ac.uk>
2022-03-20 16:15:19 +0000

    updated partial

--- api/employee/serializers.py
+++ api/employee/serializers.py
@@ -1,7 +1,13 @@
 from .models import Application
+from .models import Favourite
 from rest_framework import serializers
 
 class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
+        fields = '__all__'
+
+class FavouriteSerializer(serializers.FavouriteSerializer):
+    class Meta:
+        model = Favourite
         fields = '__all__'
--- api/employee/views.py
+++ api/employee/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from distutils.command.install_egg_info import safe_name
+from email.mime import application
+import imp
+import json
+from math import ceil
+from os import stat
+from tkinter.messagebox import RETRY
+from turtle import st
+from urllib import response
+from .models import Application
+from .models import Favourite
+from rest_framework import status
+from django.db.models import Count, Q
+from .serializers import ApplicationSerializer
+from .serializers import FavouriteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['POST'])
+def postFavourite(request):
+    if request.method == 'POST':
+        serializer = FavouriteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def postApplication(request):
+    if request.method == 'POST':
+        serializer = ApplicationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
