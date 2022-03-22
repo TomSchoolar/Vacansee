44bf6498907ea575867c8aee83bd0225b3d3ca89
Thomas Schoolar <txs090@cs.bham.ac.uk>
2022-03-22 09:00:45 +0000

    removed get request

--- api/employee/views.py
+++ api/employee/views.py
@@ -32,7 +32,7 @@ def postApplication(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST', 'GET'])
+@api_view(['POST'])
 def postFavourite(request):
     if request.method == 'POST':
         serializer = FavouriteSerializer(data=request.data)
