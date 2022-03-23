03c84b3920a69d10df28dd0bc4c45eae3d38f9b7
sapphyree <sonexix@gmail.com>
2022-03-23 22:36:56 +0000

    Add api request functionality to match components.

--- client/src/components/employer/match/MatchesColumn.vue
+++ client/src/components/employer/match/MatchesColumn.vue
@@ -1,15 +1,102 @@
 <script setup>
-    import MatchCard from '@/components/employer/review/MatchCard.vue';
+    import MatchCard from '@/components/employer/match/MatchCard.vue';
 
-    import { ref } from 'vue';
+    import { jwtGetId } from '@/assets/js/jwt';
+    import { ref, onMounted, watch, toRef } from 'vue';
+    import axios from 'axios';
 
-    let { matches = [], numMatches = 0 } = defineProps(['matches', 'numMatches'])
+    const emits = defineEmits(["show-application"])
+    const selectedProfile = ref();
 
-    const sortMatches = ref('dateDesc');
+    const props = defineProps(['selectedVacancy']);
+
+    const selectedVacancy = ref(0);
+    const selected = toRef(props, 'selectedVacancy');
+
+    const matches = ref([]);
+    const numMatches = ref(0);
+
+    const sort = ref('dateDesc');
 
     const download = () => {
         alert("downloaded!");
     }
+
+    // api request function
+    const getMatches = async (options) => {
+        const { sort = 'dateDesc', vID = selected.value  } = options;
+
+        const uID = jwtGetId(window.localStorage.jwt);
+
+        const response = await axios({
+            method: 'get',
+            url: '/e/match/matches/',
+            baseURL: process.env.VUE_APP_API_ENDPOINT,
+            responseType: 'json',
+            params: {
+                uID,
+                vID,
+                sort,
+            }
+        }).catch((err) => {
+            console.log(`oops ${ err }`);
+        });
+
+        if(!response || !response.data)
+            return false;
+
+        const { data } = response;
+
+        if(!data)
+            return false;
+
+        const {
+            matches: newMatches = matches.value,
+            numMatches: total = 0,
+        } = data;
+
+        numMatches.value = total;
+        matches.value = newMatches;
+
+        return true;
+    }
+
+    // sort vac
+    const sortMatches = async (sortParam) => {
+        const result = await getMatches({ sort: sortParam });
+
+        if(!result) {
+            alert('uh oh! something went wrong :(');
+            return;
+        }
+
+        sort.value = sortParam;
+    }
+
+    const updateMatches = async (newVac) => {
+        const result = await getMatches({ sort: sort.value, vID: newVac})
+
+        if(!result) {
+            alert('uh oh! something went wrong :(');
+            return;
+        }
+
+        selectedVacancy.value = newVac;
+    }
+
+    const updateCard = (nextProfile) => {
+        console.log(`Current Profile: ${ nextProfile.FirstName }`);
+        emits("show-application", nextProfile);
+    }
+
+    watch(selectedVacancy, updateMatches);
+
+    watch(selected, (value) => {
+        selectedVacancy.value = selected.value;
+    });
+
+    watch(sort, sortMatches);
+
 </script>
 
 <template>
@@ -32,7 +119,7 @@
                         <label for='sort-matches'>sort by:</label>
                     </div>
 
-                    <select v-model='sortMatches' aria-label='sort matches' id='sort-matches' class='sort'>
+                    <select v-model='sort' aria-label='sort matches' id='sort-matches' class='sort'>
                         <option value='dateDesc' selected>latest first</option>
                         <option value='dateAsc'>oldest first</option>
                         <option value='titleAsc'>title (a-z)</option>
@@ -42,9 +129,9 @@
             </div>
         </div>
         <div class='matches'>
-            <h3 class='no-matches' v-if='numMatches == 0'>No matches to display</h3>
+            <h3 class='no-matches' v-if='numMatches == 0'>No matches to display. </h3>
             <div class='match' v-else v-for='match in matches' :key='match'>
-                <MatchCard :stats='match' />
+                <MatchCard :stats='match' @showApplication='updateCard'/>
             </div>
         </div>
     </section>
