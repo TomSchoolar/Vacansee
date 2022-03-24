159416788d9f5b9a98d8fcf919727bfba00808b8
Cavan Day <cxd066@cs.bham.ac.uk>
2022-03-24 00:12:05 +0000

    Add employee URLs

--- api/api/urls.py
+++ api/api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
+    path('', include('employee.urls')),
     path('e/', include('employer.urls')),
 ]
