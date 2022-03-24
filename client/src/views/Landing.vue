df3c697e37510683218c57b4987a9553590f8e11
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-24 22:59:50 +0000

    Add login button

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -2,10 +2,28 @@
     <main class='container'>
         <section class='landing-page'>
             <h1>Landing Page</h1>
+            <div class='login'>
+                <router-link :to='`/login`' class='login-button'>Login</router-link>
+            </div>
         </section>
     </main>
 </template>
 
 <style>
+    .login {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .login-button {
+        width: 200px;
+        border: 2px solid black;
+        border-radius: 5px;
+        font-size: 24px;
+        background-color: var(--slate);
+        color: white;
+        text-decoration: none;
+    }
 
 </style>
