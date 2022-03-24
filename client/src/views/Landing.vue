45d722775da667c5d21a9e3ebd25ccf0ccef45ca
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-24 22:44:04 +0000

    Remove login button from landing page

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -3,26 +3,9 @@
         <section class='landing-page'>
             <h1>Landing Page</h1>
         </section>
-        <div class='login'>
-            <router-link :to='`/login`' class='login-button'>Login</router-link>
-        </div>
     </main>
 </template>
 
 <style>
-    .login {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
 
-    .login-button {
-        width: 200px;
-        border: 2px solid black;
-        border-radius: 5px;
-        font-size: 24px;
-        background-color: var(--slate);
-        color: white;
-        text-decoration: none;
-    }
 </style>
