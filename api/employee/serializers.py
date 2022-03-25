79c2ecc0b826b76c3edf3cf179b5bcd0b9f428e4
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-25 14:31:30 +0000

    Fix missing import in employee serializers

--- api/employee/serializers.py
+++ api/employee/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Application, Favourite, Reject
+from .models import Application, Favourite, Profile, Reject
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
