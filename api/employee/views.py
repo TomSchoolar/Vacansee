794f45df4b36ff37cee85cf402817d8f26eeb039
Adam <40166384+Xsychic@users.noreply.github.com>
2022-03-25 17:20:54 +0000

    Refactor and build out employer decision api routes

--- api/employee/views.py
+++ api/employee/views.py
@@ -103,34 +103,170 @@ def getIndex(request):
 
 @api_view(['POST'])
 def postApplication(request):
-    if request.method == 'POST':
-        serializer = ApplicationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        jwt = jwtHelper.extractJwt(request)
+    except jwtLib.ExpiredSignatureError:
+        return Response({ 'status': 401, 'message': 'Expired auth token' }, status=status.HTTP_401_UNAUTHORIZED)
+    except (jwtLib.InvalidKeyError, jwtLib.InvalidSignatureError, jwtLib.InvalidTokenError) as err:
+        return Response({ 'status': 401, 'message': 'Invalid auth token' }, status=status.HTTP_401_UNAUTHORIZED)
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error acquiring auth token' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        vacancyId = request.data['VacancyId']
+
+        vacancy = Vacancy.objects.get(pk = vacancyId, IsOpen__exact = True)
+
+    except KeyError:
+        return Response({ 'status': 400, 'message': 'Missing vacancy id from request' }, status=status.HTTP_400_BAD_REQUEST)
+    except Vacancy.DoesNotExist:
+        return Response({ 'status': 400, 'message': 'That vacancy is not open for applications' }, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error getting vacancy details' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        newApplication = {
+            'VacancyId': vacancy.VacancyId,
+            'UserId': jwt['id'],
+            'ApplicationStatus': 'PENDING'
+        }
+
+        serializer = ApplicationSerializer(data = newApplication)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()
+
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error while saving favourite' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    
+    try:
+        
+        newVacancySet = Vacancy.objects.filter(IsOpen__exact = True)[0]
+        newVacancy = VacancySerializer(newVacancySet).data
+        
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error getting next vacancy' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(newVacancy)
 
 
 
 @api_view(['POST'])
 def postFavourite(request):
-    if request.method == 'POST':
-        serializer = FavouriteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        jwt = jwtHelper.extractJwt(request)
+    except jwtLib.ExpiredSignatureError:
+        return Response({ 'status': 401, 'message': 'Expired auth token' }, status=status.HTTP_401_UNAUTHORIZED)
+    except (jwtLib.InvalidKeyError, jwtLib.InvalidSignatureError, jwtLib.InvalidTokenError) as err:
+        return Response({ 'status': 401, 'message': 'Invalid auth token' }, status=status.HTTP_401_UNAUTHORIZED)
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error acquiring auth token' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        vacancyId = request.data['VacancyId']
+
+        vacancy = Vacancy.objects.get(pk = vacancyId, IsOpen__exact = True)
+
+    except KeyError:
+        return Response({ 'status': 400, 'message': 'Missing vacancy id from request' }, status=status.HTTP_400_BAD_REQUEST)
+    except Vacancy.DoesNotExist:
+        return Response({ 'status': 400, 'message': 'That vacancy is not open for applications' }, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error getting vacancy details' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        newFavourite = {
+            'VacancyId': vacancy.VacancyId,
+            'UserId': jwt['id']
+        }
+
+        serializer = FavouriteSerializer(data = newFavourite)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()        
+    
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error while saving favourite' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    
+    try:
+        
+        newVacancySet = Vacancy.objects.filter(IsOpen__exact = True)[0]
+        newVacancy = VacancySerializer(newVacancySet).data
+        
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error getting next vacancy' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(newVacancy)
 
 
 
 @api_view(['POST'])
 def postReject(request):
-    if request.method == 'POST':
-        serializer = RejectSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        jwt = jwtHelper.extractJwt(request)
+    except jwtLib.ExpiredSignatureError:
+        return Response({ 'status': 401, 'message': 'Expired auth token' }, status=status.HTTP_401_UNAUTHORIZED)
+    except (jwtLib.InvalidKeyError, jwtLib.InvalidSignatureError, jwtLib.InvalidTokenError) as err:
+        return Response({ 'status': 401, 'message': 'Invalid auth token' }, status=status.HTTP_401_UNAUTHORIZED)
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error acquiring auth token' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        vacancyId = request.data['VacancyId']
+
+        vacancy = Vacancy.objects.get(pk = vacancyId, IsOpen__exact = True)
+
+    except KeyError:
+        return Response({ 'status': 400, 'message': 'Missing vacancy id from request' }, status=status.HTTP_400_BAD_REQUEST)
+    except Vacancy.DoesNotExist:
+        return Response({ 'status': 400, 'message': 'That vacancy is not open for applications' }, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error getting vacancy details' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        newReject = {
+            'VacancyId': vacancy.VacancyId,
+            'UserId': jwt['id']
+        }
+
+        serializer = RejectSerializer(data = newReject)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()        
+    
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error while saving favourite' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    
+    try:
+        
+        newVacancySet = Vacancy.objects.filter(IsOpen__exact = True)[0]
+        newVacancy = VacancySerializer(newVacancySet).data
+        
+    except Exception as err:
+        print(f'uh oh: { err }')
+        return Response({ 'status': 500, 'message': 'Error getting next vacancy' }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(newVacancy)
 
 
 
