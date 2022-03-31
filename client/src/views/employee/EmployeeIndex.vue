eb122be56f1039e29a8af969720134a6b1da3c92
Adam <40166384+Xsychic@users.noreply.github.com>
2022-04-01 00:12:51 +0100

    Create token pair on login, use api auth on all routes, create RefreshToken table

--- client/src/views/employee/EmployeeIndex.vue
+++ client/src/views/employee/EmployeeIndex.vue
@@ -61,6 +61,9 @@
             method: 'get',
             url: '/vacancy/',
             responseType: 'json',
+            headers: {
+                Authorization: `Bearer: ${ token }`
+            },
             params: {
                 sort,
                 count,
