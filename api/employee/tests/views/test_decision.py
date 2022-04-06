8c4fc6752ecd81e07500019c702e6564630d3a64
Adam <40166384+Xsychic@users.noreply.github.com>
2022-04-06 16:20:33 +0100

    Refactor authentication tests and fix them wrt auth changes

--- api/employee/tests/views/test_decision.py
+++ api/employee/tests/views/test_decision.py
@@ -1,10 +1,9 @@
 import environ
 import jwt as jwtLib
 from django.test import TestCase
-from authentication import jwt as jwtHelper
+from authentication.helpers import jwt as jwtHelper
 from datetime import datetime, timezone, timedelta
 from employee.models import Favourite, Application, Reject
-from employee.serializers import FavouriteSerializer, ApplicationSerializer, RejectSerializer
 
 
 env = environ.Env()
