02949df24c28252b9347e097da23dd28d48843e6
Thomas Schoolar <txs090@cs.bham.ac.uk>
2022-04-08 10:39:49 +0100

    connected page to api

--- api/employee/urls.py
+++ api/employee/urls.py
@@ -4,11 +4,11 @@ from django.urls import path
 urlpatterns = [
     path('vacancy/', views.getIndex),
     path('vacancy/fav/', views.postFavourite),
-    path('vacancy/favourites/', views.favourites),
     path('vacancy/apply/', views.postApplication),
     path('vacancy/reject/', views.postReject),
     path('applications/', views.getApplications),
     path('applications/stats/', views.getApplicationStats),
     path('applications/delete/<int:applicationId>/', views.deleteApplication),
-    path('applications/<int:applicationId>/', views.getApplicationDetails)
+    path('applications/<int:applicationId>/', views.getApplicationDetails),
+    path('favourites/', views.getFavourites)
 ]
