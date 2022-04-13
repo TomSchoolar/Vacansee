5337065153c5e4f84cd2e1df213f2a2cce68c171
Adam <adam.bass54@yahoo.co.uk>
2022-04-13 15:54:46 +0100

    Fix expanding text field and add logistics page with new field types to employer new vacancy form

---
+++ client/src/components/employer/newVacancy/formComponents/FormDropdown.vue
@@ -0,0 +1,51 @@
+<script setup>
+    const props = defineProps(['name', 'label'])
+</script>
+
+<template>
+    <div class='form-group'>
+        <label :for='props.name' class='label'>{{ props.label }}:</label>
+        <select 
+            :name='props.name' 
+            class='input'
+            :id='props.name' 
+            required
+        >
+            <option value='' selected hidden disabled>select a timezone</option>
+            <option value='all'>all</option>
+            <option value='the'>the</option>
+            <option value='different'>different</option>
+            <option value='timezones'>timezones</option>
+        </select>
+    </div>
+</template>
+
+<style scoped>
+    select:required:invalid {
+        color: var(--slate);
+    }
+
+    option {
+        color: black;
+    }
+
+    .form-group {
+        display: flex;
+        flex-direction: column;
+        width: calc(100% - 8px);
+        margin: 0 4px 25px 4px;
+    }
+
+    .input {
+        padding: 4px 2px;
+        font-size: 16px;
+    }
+
+    .label {
+        font-weight: bold;
+        font-size: 11px;
+        margin: 0;
+        position: relative;
+        left: 1px;
+    }
+</style>
--- client/src/components/employer/newVacancy/formComponents/FormExpandingDoubleText.vue
+++ client/src/components/employer/newVacancy/formComponents/FormExpandingDoubleText.vue
@@ -1,7 +1,7 @@
 <script setup>
     import { onMounted, ref } from 'vue';
 
-    const props = defineProps(['name', 'label', 'placeholder', 'max'])
+    const props = defineProps(['name', 'label', 'placeholder', 'secondPlaceholder', 'max'])
 
     let fields = [];
     let activeFields = [];
@@ -30,9 +30,9 @@
             let nextInputNum = ( inputNum + 1 <= props.max ? inputNum + 1 : false );
             let nextInput = document.querySelector(`#${ id[0] }-${ nextInputNum }`);
 
-            if(value && nextInputNum && nextInput.classList.contains('input-hidden')) {
+            if(value && nextInputNum && nextInput.parentElement.classList.contains('input-hidden')) {
                 // field has been populated, add another field if not at max
-                nextInput.classList.remove('input-hidden');
+                nextInput.parentElement.classList.remove('input-hidden');
                 activeFields.push(nextInput);
             }
 
@@ -45,7 +45,9 @@
                     shuffleInputValues(inputNum)
                 } else {
                     // if the next field is last, it can just be removed
-                    activeFields.pop().classList.add('input-hidden')
+                    let el = activeFields.pop();
+                    el.parentElement.classList.add('input-hidden');
+                    el.nextElementSibling.value = '';
                 }
             }
         };
@@ -55,11 +57,13 @@
         for(let i = --startIndex; i + 2 < activeFields.length; i++) {
             // move fields values up two places
             activeFields[i].value = activeFields[i+2].value;
+            activeFields[i].nextElementSibling.value = activeFields[i+2].nextElementSibling.value;
             activeFields[i+2].value = '';
+            activeFields[i+2].nextElementSibling.value = '';
         }
         
         // remove last input from form and active fields array
-        activeFields.pop().classList.add('input-hidden');
+        activeFields.pop().parentElement.classList.add('input-hidden');
     }
 
 
@@ -68,19 +72,37 @@
 <template>
     <div class='form-group' :id='`expanding-${ props.name }`'>
         <label :for='`${ props.name }-1`' class='label'>{{ props.label }}:</label>
-        <input 
-            v-for='i in props.max'
-            :key='i' 
-            type='text' 
-            :name='`${ props.name }-${ i }`' 
-            :class='"input " + `input-${ props.name }` + " " + ( i <= numFields ? "" : "input-hidden" )'
-            :id='`${ props.name }-${ i }`' 
-            :placeholder='props.placeholder ? props.placeholder : "" ' 
-        >
+        <div :class='"double-input-container" + " " + ( i <= numFields ? "" : "input-hidden" )' v-for='i in props.max' :key='i'>
+            <input 
+                type='text' 
+                :name='`${ props.name }-${ i }`' 
+                :class='"input " + `input-${ props.name }`'
+                :id='`${ props.name }-${ i }`' 
+                :placeholder='props.placeholder ? props.placeholder : "" ' 
+            >
+            <input 
+                type="text"
+                :name='`${ props.name }-${ i }-time`'
+                :class='"input input-time " + `input-${ props.name }-time`'
+                :id='`${ props.name }-${ i }`' 
+                :placeholder='props.secondPlaceholder ? props.secondPlaceholder : "" ' 
+            >
+        </div>
     </div>
+
 </template>
 
 <style scoped>
+    .double-input-container {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
+
+    .double-input-container:not(:last-of-type) {
+        margin-bottom: 12px;
+    }
+
     .form-group {
         display: flex;
         flex-direction: column;
@@ -91,14 +113,18 @@
     .input {
         padding: 4px 2px;
         font-size: 16px;
-    }
-
-    .input:not(:last-of-type) {
-        margin-bottom: 12px;
+        width: 65px;
+        flex-grow: 1;
     }
 
     .input-hidden {
         display: none;
+    }
+
+    .input-time {
+        margin-left: 10px;
+        width: 30%;
+        flex-grow: 0;
     }
 
     .label {
