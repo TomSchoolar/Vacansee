7f1e347e6e2db3b6c3c5622cd01b234229fd9b43
Thomas Schoolar <txs090@cs.bham.ac.uk>
2022-04-13 13:16:15 +0100

    Basic modal

---
+++ client/src/components/employer/review/AreYouSureModal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="modal-overlay" @click="$emit('close-modal')">
+    <div class="modal">
+      <h6>Are You Sure you want to unmatch with</h6>
+      <button @click="$emit('close-modal')">No</button>
+      <button @click="$emit('unmatch')">Yes</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+}
+</script>
+
+<style scoped>
+
+.modal-overlay {
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+}
+
+.modal {
+  text-align: center;
+  background-color: white;
+  height: 200px;
+  width: 400px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+}
+.close {
+  margin: 10% 0 0 16px;
+  cursor: pointer;
+}
+
+.close-img {
+  width: 25px;
+}
+
+.check {
+  width: 150px;
+}
+
+h6 {
+  font-weight: 500;
+  font-size: 28px;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 16px;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #ac003e;
+  width: 150px;
+  height: 40px;
+  color: white;
+  font-size: 14px;
+  border-radius: 16px;
+  margin-top: 50px;
+}
+</style>
