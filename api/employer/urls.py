ce84d917ce156a3c48500523f5bac3b6c53caa1f
Adam <adam.bass54@yahoo.co.uk>
2022-04-14 19:00:08 +0100

    Rename matches employer views file for consistency

--- api/employer/urls.py
+++ api/employer/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import account, index, match, review
+from .views import account, index, matches, review
 
 urlpatterns = [
     path('vacancy/', index.getIndex),
     path('vacancy/stats/', index.getIndexStats),
     path('review/<int:vacancyId>/', review.getReview),
     path('review/<int:vacancyId>/updatestatus/<int:applicationId>/', review.putReviewApplication),
-    path('match/', match.getMatchVacancies),
-    path('match/matches/', match.getMatches),
-    path('match/card/', match.getCard),
+    path('match/', matches.getMatchVacancies),
+    path('match/matches/', matches.getMatches),
+    path('match/card/', matches.getCard),
     path('account/', account.getAccount),
     path('account/update/', account.putAccount),
     path('account/delete/', account.deleteAccount)
