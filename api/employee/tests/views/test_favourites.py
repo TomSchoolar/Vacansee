593d67aebbc63c58aaad697636ab01ef268698d4
sapphyree <sonexix@gmail.com>
2022-04-14 23:41:17 +0100

    Add additional employee testing

--- api/employee/tests/views/test_favourites.py
+++ api/employee/tests/views/test_favourites.py
@@ -1,20 +1,175 @@
+from math import ceil
 from django.test import TestCase
+from employer.serializers import VacancySerializer
+from employer.models import Vacancy, EmployerDetails
 from employee.models import Favourite
 from authentication.tests.jwtFuncs import createAccessToken
 
+class getFavouritesTests(TestCase):
 
-
-class favouritesTestCase(TestCase):
-
+    userId = 2
+    jwt = createAccessToken(userId)
     fixtures = ['authentication/fixtures/testseed.json']
-    jwt = createAccessToken(2)
 
-    def test_getFavourites(self):
-        response = self.client.get('/favourites/', { 'uID':2, 'sort':'titleAsc', 'count':5, 'pageNum':1 }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+    def test_validRequestSortDateDesc(self):
+        response = self.client.get('/favourites/', { 'sort':'dateDesc', 'count':5, 'pageNum':1 }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
 
-        favouriteSet = Favourite.objects.filter(
-            UserId__exact = 2
-        )
+        favouriteSet = Favourite.objects.filter(UserId__exact = self.userId)
+        vacancyIds = []
+        for fav in favouriteSet:
+            vacancyIds.append(int(fav.VacancyId.VacancyId))
+
+        vacanciesSet = Vacancy.objects.filter(VacancyId__in = vacancyIds).order_by('-Created')[0:5]
+        numVacancies = Vacancy.objects.filter(VacancyId__in = vacancyIds).count()
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['numVacancies'], len(favouriteSet))
+        self.assertDictEqual(expectedData, response.data)
+
+
+    def test_validRequestSortDateAsc(self):
+        response = self.client.get('/favourites/', { 'sort':'dateAsc', 'count':5, 'pageNum':1 }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+        
+        favouriteSet = Favourite.objects.filter(UserId__exact = self.userId)
+        vacancyIds = []
+        for fav in favouriteSet:
+            vacancyIds.append(int(fav.VacancyId.VacancyId))
+
+        vacanciesSet = Vacancy.objects.filter(VacancyId__in = vacancyIds).order_by('Created')[0:5]
+        numVacancies = Vacancy.objects.filter(VacancyId__in = vacancyIds).count()
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(expectedData, response.data)
+
+
+    def test_validRequestSortTitleAsc(self):
+        response = self.client.get('/favourites/', { 'sort':'titleAsc', 'count':5, 'pageNum':1 }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+        
+        favouriteSet = Favourite.objects.filter(UserId__exact = self.userId)
+        vacancyIds = []
+        for fav in favouriteSet:
+            vacancyIds.append(int(fav.VacancyId.VacancyId))
+
+        vacanciesSet = Vacancy.objects.filter(VacancyId__in = vacancyIds).order_by('VacancyName')[0:5]
+        numVacancies = Vacancy.objects.filter(VacancyId__in = vacancyIds).count()
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(expectedData, response.data)
+
+
+    def test_incorrectlyLargePageNumSortTitleDesc(self):
+        response = self.client.get('/favourites/', { 'sort':'titleDesc', 'count':5, 'pageNum':3 }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        favouriteSet = Favourite.objects.filter(UserId__exact = self.userId)
+        vacancyIds = []
+        for fav in favouriteSet:
+            vacancyIds.append(int(fav.VacancyId.VacancyId))
+
+        vacanciesSet = Vacancy.objects.filter(VacancyId__in = vacancyIds).order_by('-VacancyName')[5:10]
+        numVacancies = Vacancy.objects.filter(VacancyId__in = vacancyIds).count()
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(expectedData, response.data)
+
+    def test_missingParameters(self):
+        response = self.client.get('/favourites/', { }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_expiredJWT(self):
+        jwt = createAccessToken(self.jwt, 'now')
+
+        response = self.client.get('/favourites/', { 'sort':'titleAsc', 'count':5, 'pageNum':1 }, **{'HTTP_AUTHORIZATION': f'Bearer: { jwt }'})
+
+        self.assertEqual(response.data['status'], 401)
+        self.assertEqual(response.data['message'], 'Expired auth token')
+
+    def test_invalidJWT(self):
+        jwt = self.jwt[:-1]
+
+        response = self.client.get('/favourites/', { 'sort':'titleAsc', 'count':5, 'pageNum':1 }, **{'HTTP_AUTHORIZATION': f'Bearer: { jwt }'})
+
+        self.assertEqual(response.data['status'], 401)
+        self.assertEqual(response.data['message'], 'Invalid auth token')
+
+class postFavouritesTests(TestCase):
+    
+    vacId = 1000
+    userId = 2
+    jwt = createAccessToken(userId)
+    fixtures = ['authentication/fixtures/testseed.json']
+    
+    def test_validRequest(self):
+        response = self.client.post('/vacancy/fav/', { 'VacancyId': self.vacId }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        self.assertEqual(response.status_code, 201)      
+
+    def test_invalidVacancy(self):
+        response = self.client.post('/vacancy/fav/', { 'VacancyId': 9999 }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        self.assertEqual(response.data['status'], 400)
+        self.assertEqual(response.data['message'], 'That vacancy is not open for applications')
+
+    def test_missingParameters(self):
+        response = self.client.post('/vacancy/fav/', { }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_expiredJWT(self):
+        jwt = createAccessToken(self.jwt, 'now')
+
+        response = self.client.post('/vacancy/fav/', { 'VacancyId': self.vacId }, **{'HTTP_AUTHORIZATION': f'Bearer: { jwt }'})
+
+        self.assertEqual(response.data['status'], 401)
+        self.assertEqual(response.data['message'], 'Expired auth token')
+
+    def test_invalidJWT(self):
+        jwt = self.jwt[:-1]
+
+        response = self.client.post('/vacancy/fav/', { 'VacancyId': self.vacId }, **{'HTTP_AUTHORIZATION': f'Bearer: { jwt }'})
+
+        self.assertEqual(response.data['status'], 401)
+        self.assertEqual(response.data['message'], 'Invalid auth token')
--- api/employee/tests/views/test_index.py
+++ api/employee/tests/views/test_index.py
@@ -1,4 +1,6 @@
+from math import ceil
 from django.test import TestCase
+from employer.serializers import VacancySerializer, EmployerDetails
 from employer.models import Vacancy
 from authentication.tests.jwtFuncs import createAccessToken
 from employee.models import Favourite, Reject, Application
@@ -10,8 +12,7 @@ class applicationTestCase(TestCase):
     fixtures = ['authentication/fixtures/testseed.json']
     jwt = createAccessToken(1019)
 
-    def test_getApplications(self):
-        response = self.client.get('/vacancy/', { 'uID':1019, 'sort':'titleAsc', 'count':5, 'pageNum':1, 'filter':'all' }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+    def getVacancyList(self):
 
         vacancyList = []
 
@@ -36,7 +37,117 @@ class applicationTestCase(TestCase):
         for app in applicationSet:
             vacancyList.append(app.VacancyId.VacancyId)
 
-        vacancySet = Vacancy.objects.all().exclude(VacancyId__in = vacancyList)
+        return vacancyList
+
+    def test_validRequestSortTitleAscFilterAll(self):
+        response = self.client.get('/vacancy/', { 'uID':1019, 'sort':'titleAsc', 'count':5, 'pageNum':1, 'filter':'all' }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        vacancyList = self.getVacancyList()
+
+        numVacancies = Vacancy.objects.filter(IsOpen__in = [True, False]).exclude(VacancyId__in = vacancyList).count()
+
+        vacanciesSet = Vacancy.objects.filter(IsOpen__in = [True, False]).exclude(VacancyId__in = vacancyList).order_by('VacancyName')[0:5] 
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual((response.data['numVacancies']), len(vacancySet))
+        self.assertDictEqual(expectedData, response.data)
+
+    def test_validRequestSortTitleDescFilterClosed(self):
+        response = self.client.get('/vacancy/', { 'uID':1019, 'sort':'titleDesc', 'count':5, 'pageNum':1, 'filter':'closed' }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        vacancyList = self.getVacancyList()
+
+        numVacancies = Vacancy.objects.filter(IsOpen__in = [False]).exclude(VacancyId__in = vacancyList).count()
+
+        vacanciesSet = Vacancy.objects.filter(IsOpen__in = [False]).exclude(VacancyId__in = vacancyList).order_by('-VacancyName')[0:5] 
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(expectedData, response.data) 
+
+    def test_validRequestSortDateDescFilterActive(self):
+        response = self.client.get('/vacancy/', { 'uID':1019, 'sort':'dateDesc', 'count':5, 'pageNum':1, 'filter':'active' }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        vacancyList = self.getVacancyList()
+
+        numVacancies = Vacancy.objects.filter(IsOpen__in = [True]).exclude(VacancyId__in = vacancyList).count()
+
+        vacanciesSet = Vacancy.objects.filter(IsOpen__in = [True]).exclude(VacancyId__in = vacancyList).order_by('-Created')[0:5] 
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(expectedData, response.data) 
+
+    def test_incorrectlyLargePageNumSortDateAsc(self):
+        response = self.client.get('/vacancy/', { 'uID':1019, 'sort':'dateAsc', 'count':5, 'pageNum':5, 'filter':'all' }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        vacancyList = self.getVacancyList()
+
+        numVacancies = Vacancy.objects.filter(IsOpen__in = [True, False]).exclude(VacancyId__in = vacancyList).count()
+
+        vacanciesSet = Vacancy.objects.filter(IsOpen__in = [True, False]).exclude(VacancyId__in = vacancyList).order_by('Created')[10:15] 
+        vacancies = VacancySerializer(vacanciesSet, many=True).data
+
+        for vacancy in vacancies:
+            employerDetails = EmployerDetails.objects.get(UserId__exact = vacancy['UserId'])
+            vacancy['CompanyName'] = employerDetails.CompanyName
+
+        expectedData = {
+            'numPages': ceil(numVacancies / 5),
+            'vacancies': vacancies,
+            'numVacancies': numVacancies
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertDictEqual(expectedData, response.data) 
+
+    def test_missingParameters(self):
+        response = self.client.get('/vacancy/', { }, **{'HTTP_AUTHORIZATION': f'Bearer: { self.jwt }'})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_expiredJWT(self):
+        jwt = createAccessToken(self.jwt, 'now')
+
+        response = self.client.get('/vacancy/', { 'sort':'titleAsc', 'count':5, 'pageNum':1, 'filter':'all' }, **{'HTTP_AUTHORIZATION': f'Bearer: { jwt }'})
+
+        self.assertEqual(response.data['status'], 401)
+        self.assertEqual(response.data['message'], 'Expired auth token')
+
+    def test_invalidJWT(self):
+        jwt = self.jwt[:-1]
+
+        response = self.client.get('/vacancy/', { 'sort':'titleAsc', 'count':5, 'pageNum':1, 'filter':'all' }, **{'HTTP_AUTHORIZATION': f'Bearer: { jwt }'})
+
+        self.assertEqual(response.data['status'], 401)
+        self.assertEqual(response.data['message'], 'Invalid auth token')
