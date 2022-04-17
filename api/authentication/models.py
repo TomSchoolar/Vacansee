ff1b9a5296b92f6427b21e2000bfe5f5ce9fc71e
sapphyree <sonexix@gmail.com>
2022-04-17 13:46:12 +0100

    Fix DB model callable issue

--- api/authentication/models.py
+++ api/authentication/models.py
@@ -19,5 +19,5 @@ class RefreshToken(models.Model):
     TokenId = models.AutoField(primary_key=True)
     FamilyId = models.IntegerField(null=False)
     Token = models.CharField(max_length=200, null=False, unique=True)
-    Expiry = models.DateTimeField(null=False, default=getAccessExpiry())
+    Expiry = models.DateTimeField(null=False, default=getAccessExpiry)
     IsLatest = models.BooleanField(null=False, default=True)
