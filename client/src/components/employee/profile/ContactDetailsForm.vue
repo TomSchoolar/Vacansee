a785e8042b26b35315d5827648b50262ccc4f242
sxd110 <sxd110@student.bham.ac.uk>
2022-04-25 13:57:58 +0100

    remove email form component

--- client/src/components/employee/profile/ContactDetailsForm.vue
+++ client/src/components/employee/profile/ContactDetailsForm.vue
@@ -5,14 +5,12 @@
     const validate = () => {
         // define schema
         const schema = Joi.object({
-            'phonenum': Joi.string().min(8).max(30).required().label('phonenum'),
-            'email': Joi.string().email({ tlds: { allow: false } }).max(254).required().label('email'),
+            'PhoneNumber': Joi.string().min(8).max(30).required().label('PhoneNumber'),
         });
 
         // get input data
         const data = {
-            'phonenum': document.querySelector('input[name="phonenum"]').value,
-            'email': document.querySelector('input[name="email"]').value,
+            'PhoneNumber': document.querySelector('input[name="PhoneNumber"]').value,
         }
 
         // validate and handle any errors
@@ -24,13 +22,11 @@
 
 <template>
     <h1> Contact Details </h1> 
-    <p> We need your contact details next. Enter a current phone number and email address </p>
-    <form>
-        <label for='phonenum'>Phone number:</label><br />
-        <input type='text' id='phonenum' name='phonenum'><br />
-        <label for='email'>Email address:</label><br />
-        <input type='text' id='email' name='email'>
-    </form>
+    <p> We need your contact details next. Enter a current phone number and Email address </p>
+    <div id='myform'>
+        <label for='PhoneNumber'>Phone number:</label><br />
+        <input type='text' id='PhoneNumber' name='PhoneNumber'><br />
+    </div>
 
     <FormButtons :back='true' :next='true' @back='emit("back")' @next='emit("next")' />
 </template>
