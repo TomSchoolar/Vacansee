d0abecf12b79743d8bbd44dd5e8a888d388d31f6
Cavan Day <cxd066@cs.bham.ac.uk>
2022-04-26 12:34:42 +0000

    Create user ID generator helper function

--- api/authentication/helpers/auth.py
+++ api/authentication/helpers/auth.py
@@ -1,5 +1,7 @@
 from employee.models import Profile
 from employer.models import EmployerDetails
+from random import randint
+from ..models import User
 
 
 def getEmployeeDetails(userId):
@@ -13,4 +15,17 @@ def getEmployerDetails(userId):
         'CompanyName': details.CompanyName,
         'PhoneNumber': details.PhoneNumber
     }
+
+
+def generateUserId(body):
+    userId = randint(1,10000)
+
+    try:
+        userObj = User.objects.get(UserId__exact = userId)
+        generateUserId(body)
+    except User.DoesNotExist:
+        return userId
+    except Exception as err:
+        print(err)
+        generateUserId(body)
 
