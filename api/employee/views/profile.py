802d251ae47065ce772f888c97d99c15c4bf1ea5
sapphyree <sonexix@gmail.com>
2022-05-03 18:00:46 +0100

    Fix API route

--- api/employee/views/profile.py
+++ api/employee/views/profile.py
@@ -3,8 +3,10 @@ from rest_framework import status
 from authentication.models import User
 from rest_framework.response import Response
 from ..models import Profile
+from ..forms import ProfileForm
 from rest_framework.decorators import api_view
 from authentication.helpers import jwt as jwtHelper
+from json import loads
 
 @api_view(['POST'])
 def postProfile(request):
@@ -42,20 +44,28 @@ def postProfileEdit(request):
         return jwt
 
     try:
-        data = request.data
+        if request.data is not dict:
+            data = dict(request.data)
+        else:
+            data = request.data
     except:
         return Response({ 'status': 400 }, status=status.HTTP_400_BAD_REQUEST)
 
-    try:
-        profile = Profile.objects.filter(pk = jwt['id']).update(**data)
-        profile.full_clean()
-        profile.save()
+    profileSet = Profile.objects.get(UserId__exact = jwt['id'])
 
-        return Response({ 'status': 200 }, status=status.HTTP_200_OK)
-    except ValidationError as err:
-        return Response({ 'status': 400, 'message': str(err) }, status=status.HTTP_400_BAD_REQUEST)
+    data['NotableSkills'] = loads(data['NotableSkills'])
+    data['Experience'] = loads(data['Experience'])
+    data['Qualifications'] = loads(data['Qualifications'])
+    data['UserId'] = User.objects.get(pk = jwt['id'])
+
+    try:
+        updatedProfile = ProfileForm(data, instance=profileSet)
+        if updatedProfile.is_valid():
+            updatedProfile.save()
+            return Response({ 'status': 200 }, status=status.HTTP_200_OK)
+        else:
+            return Response({ 'status': 400, 'message': 'Invalid profile data' }, status=status.HTTP_400_BAD_REQUEST)
+
     except Exception as err:
-        import traceback
-        traceback.print_exc()
         print(f'uh oh: { err }')
         return Response({ 'status': 500, 'message': str(err) }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
