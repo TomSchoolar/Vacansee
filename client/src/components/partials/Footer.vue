096ad447552e64d28b7108bbcc1d44b1fa70ab4d
Cavan Day <cxd066@cs.bham.ac.uk>
2022-05-03 17:14:50 +0000

    Create footer partial

---
+++ client/src/components/partials/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+    <section id='footer'>
+        <span class='footer-centre-wrapper'>
+            <div class='footer-link-block'>
+                <router-link to='/privacy' class='footer-link'>Privacy Policy</router-link>
+            </div>
+        </span>
+    </section>
+</template>
+
+
+
+<style scoped>
+    .footer-active-link {
+        color: #ffffff !important;
+        font-weight: bold !important;
+    }
+
+    .footer-centre-wrapper {
+        display: flex;
+        margin: 0 auto;
+    }
+
+    .footer-link {
+        color: #ffffff;
+        text-decoration: none;
+    }
+
+    .footer-link:focus, .footer-link:hover, .footer-link:active {
+        color: #d5d5d5;
+    }
+
+    .footer-link-block {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #footer {
+        width: 60vw;
+        height: 70px;
+        padding: 0 20vw;
+        margin-top: 30px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #ffffff;
+        background: var(--slate);
+    }
+</style>
