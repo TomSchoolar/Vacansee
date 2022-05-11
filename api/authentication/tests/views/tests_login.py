17bb419f4365239d6658a6c3ddd5965cdadbd7c7
Adam <40166384+Xsychic@users.noreply.github.com>
2022-05-11 17:58:08 +0100

    Fix test

--- api/authentication/tests/views/tests_login.py
+++ api/authentication/tests/views/tests_login.py
@@ -14,6 +14,8 @@ class LoginPostTestClass(TestCase):
     employeeEmail = 'ajb042@student.bham.ac.uk'
     employerId = 4
     employerEmail = 'sxd110@student.bham.ac.uk'
+    noProfileId = 3
+    noProfileEmail = 'cxd066@student.bham.ac.uk';
 
     fixtures = ['authentication/fixtures/fixtures.json']
 
@@ -126,60 +128,13 @@ class LoginPostTestClass(TestCase):
         self.assertEquals(secondTokenFamily, 1)
 
 
-class LoginProfileCheckTestClass(TestCase):
-
-    password = 'password'
-    employeeId = 2
-    employeeEmail = 'ajb042@student.bham.ac.uk'
-    employerId = 4
-    employerEmail = 'sxd110@student.bham.ac.uk'
-    # delete the profile later
-    noProfileId = 3
-    noProfileEmail = 'cxd066@student.bham.ac.uk'
-
-    fixtures = ['authentication/fixtures/fixtures.json']
-
-
-    def tearDown(self):
-        RefreshToken.objects.all().delete()
-
-
-    def test_validLoginRequest(self):
-
-        response = self.client.post('/login/', { 'email': self.employeeEmail, 'password': self.password })
-        
-        expectedUserData = {
-            'IsEmployer': False,
-            'Email': self.employeeEmail,
-            'HasProfileSetup': True
-        }
-
-        self.assertDictEqual(response.data['userData'], expectedUserData)
-
-        accessToken = jwtLib.decode(response.data['accessToken'], env('JWT_SECRET'), algorithms=['HS256'], verify=True)
-        refreshToken = jwtLib.decode(response.data['refreshToken'], env('JWT_SECRET'), algorithms=['HS256'], verify=True)
-
-        
-        self.assertTrue('exp' in accessToken)
-        self.assertTrue('iat' in accessToken)
-        self.assertEquals(accessToken['id'], self.employeeId)
-        self.assertEquals(accessToken['typ'], 'access')
-
-        self.assertTrue('exp' in refreshToken)
-        self.assertTrue('iat' in refreshToken)
-        self.assertEquals(refreshToken['id'], self.employeeId)
-        self.assertEquals(refreshToken['typ'], 'refresh')
-
-        RefreshToken.objects.get(Token__exact = response.data['refreshToken'])
 
     def test_validNoProfileLoginRequest(self):
-
         # delete profile first.
-        profile = Profile.objects.get(UserId__exact = 3)
+        profile = Profile.objects.get(UserId__exact = self.noProfileId)
         profile.delete()
 
-        
-        response = self.client.post('/login/', { 'email': self.noProfileEmail, 'password': self.password })
+        response = self.client.post('/v1/login/', { 'email': self.noProfileEmail, 'password': self.password })
         
         expectedUserData = {
             'IsEmployer': False,
@@ -201,38 +156,6 @@ class LoginProfileCheckTestClass(TestCase):
         self.assertTrue('exp' in refreshToken)
         self.assertTrue('iat' in refreshToken)
         self.assertEquals(refreshToken['id'], self.noProfileId)
-        self.assertEquals(refreshToken['typ'], 'refresh')
-
-        RefreshToken.objects.get(Token__exact = response.data['refreshToken'])
-    
-    def test_validEmployerLoginRequest(self):
-
-        response = self.client.post('/login/', { 'email': self.employerEmail, 'password': self.password })
-        
-        details = EmployerDetails.objects.get(UserId__exact = self.employerId)
-
-        expectedUserData = {
-            'IsEmployer': True,
-            'Email': self.employerEmail,
-            'CompanyName': details.CompanyName,
-            'PhoneNumber': details.PhoneNumber,
-            'HasProfileSetup': True
-        }
-
-        self.assertDictEqual(response.data['userData'], expectedUserData)
-
-        accessToken = jwtLib.decode(response.data['accessToken'], env('JWT_SECRET'), algorithms=['HS256'], verify=True)
-        refreshToken = jwtLib.decode(response.data['refreshToken'], env('JWT_SECRET'), algorithms=['HS256'], verify=True)
-
-        
-        self.assertTrue('exp' in accessToken)
-        self.assertTrue('iat' in accessToken)
-        self.assertEquals(accessToken['id'], self.employerId)
-        self.assertEquals(accessToken['typ'], 'access')
-
-        self.assertTrue('exp' in refreshToken)
-        self.assertTrue('iat' in refreshToken)
-        self.assertEquals(refreshToken['id'], self.employerId)
         self.assertEquals(refreshToken['typ'], 'refresh')
 
         RefreshToken.objects.get(Token__exact = response.data['refreshToken'])
